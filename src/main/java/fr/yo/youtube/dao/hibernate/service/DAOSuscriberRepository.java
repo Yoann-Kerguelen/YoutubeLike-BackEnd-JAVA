f550083cb376899b6923c51e13ffded631162b15
Yok29 <104489419+Yok29@users.noreply.github.com>
2022-05-30 21:36:02 +0200

    add channel suscriber and youtuber crud

--- src/main/java/fr/yo/youtube/dao/hibernate/service/DAOSuscriberRepository.java
+++ src/main/java/fr/yo/youtube/dao/hibernate/service/DAOSuscriberRepository.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 
 import fr.yo.youtube.dao.IDAOSuscriber;
 import fr.yo.youtube.dao.hibernate.SuscriberRepository;
+import fr.yo.youtube.entities.Channel;
+import fr.yo.youtube.entities.Suscriber;
 
 /**
  * La classe sevant à récupérer le repo et l'utiliser pour les crud demander par
@@ -18,5 +20,17 @@ public class DAOSuscriberRepository implements IDAOSuscriber {
 
 	@Autowired
 	SuscriberRepository suscriberRepository;
+
+	@Override
+	public void suscribeToAChannel(Suscriber suscriber, Channel channel) {
+		suscriber.getChannelsSubscriptions().add(channel);
+		suscriberRepository.save(suscriber);
+	}
+
+	@Override
+	public void unSuscribeToAChannel(Suscriber suscriber, Channel channel) {
+		suscriber.getChannelsSubscriptions().remove(channel);
+		suscriberRepository.save(suscriber);
+	}
 
 }
